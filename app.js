4db34561cc2294063d6b5f5734b8e15339f2c895
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2018-09-24 20:07:45 +0200

    Use environment variables
    MONGODB_URI - database URL
    CLIENT_URL - URL of the React app

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const passportSetup = require("./config/passport");
 mongoose.Promise = Promise;
 mongoose
   .connect(
-    "mongodb://localhost/project-showcase-backend",
+    process.env.MONGODB_URI,
     { useMongoClient: true }
   )
   .then(() => {
@@ -38,7 +38,7 @@ app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
 
 app.use(cors({
   credentials: true,
-  origin: "http://localhost:3000",
+  origin: process.env.CLIENT_URL,
 }));
 app.use(
   session({
