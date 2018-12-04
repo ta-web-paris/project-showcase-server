665921db27426ad455a15d1391f6877344616772
Marie Gillier <magillier@hotmail.fr>
2018-12-04 17:03:49 +0100

    updated project model to link creators with each of their linkedin url

--- models/project-model.js
+++ models/project-model.js
@@ -5,14 +5,18 @@ const Schema = mongoose.Schema;
 const projectSchema = new Schema(
   {
     name: { type: String, required: true },
-    creators: [{ type: String, required: true }],
+    creators: [
+      { 
+        name : {type: String, required: true},
+        linkedInUrl: {type: String}
+      }
+    ],
     screenshotUrl: { type: String },
     description: { type: String },
     gitHubUrl: { type: String },
     projectUrl: { type: String, required: true },
     tools: [{ type: String }],
     likes: {type: Number},
-    linkedInUrl: {type:String},
     projectCredentials: [ {type: String}],
     display: {type: String, enum: ["mobile", "web"]},
     bootcamp: {type: String, enum: ["Web Dev Full Time", "Web Dev Part Time", "UX/UI Part Time", "UX/UI Full Time", "Data Analytics" ]},
