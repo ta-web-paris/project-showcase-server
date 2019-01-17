1a354bf030155a351976b22ac83847d81e680abc
abisummers <hello@abisummers.com>
2019-01-17 10:38:36 +0100

    updated admin routes

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -4,7 +4,13 @@ const router = express.Router();
 const Project = require("../models/project-model");
 
 router.get("/verified", (req, res, next) => {
-  Project.find()
+  Project.find({ verified: { $eq: "verified" } })
+    .then(projectArr => res.json(projectArr))
+    .catch(next);
+});
+
+router.get("/notverified", (req, res, next) => {
+  Project.find({ verified: { $eq: "notverified" } })
     .then(projectArr => res.json(projectArr))
     .catch(next);
 });
